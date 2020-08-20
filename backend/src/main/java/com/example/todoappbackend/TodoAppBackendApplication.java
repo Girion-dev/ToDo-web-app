74a5a0084d49d4ae58710906c0306f015af8f69a
Girion-dev <mat.hladky@gmail.com>
2020-08-20 23:47:58 +0200

    Added the barebones todo page for the frontend

--- backend/src/main/java/com/example/todoappbackend/TodoAppBackendApplication.java
+++ backend/src/main/java/com/example/todoappbackend/TodoAppBackendApplication.java
@@ -2,6 +2,9 @@ package com.example.todoappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoAppBackendApplication {
@@ -9,5 +12,4 @@ public class TodoAppBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppBackendApplication.class, args);
 	}
-
 }
--- backend/src/main/java/com/example/todoappbackend/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/com/example/todoappbackend/configuration/WebSecurityConfiguration.java
@@ -12,6 +12,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -36,6 +44,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .successHandler(loginSuccessHandler)
                 .failureHandler(new SimpleUrlAuthenticationFailureHandler());
         http.csrf().ignoringAntMatchers("/api/login").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
